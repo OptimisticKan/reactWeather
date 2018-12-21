32814364db0e1e4cb626533455be623813896334
Lee <oklcs@naver.com>
2018-12-21 14:58:47 +0900

    Markdown Setting

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I love U!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+===============================
+This is reactWeather Native App
+===============================
+
+Author : BC in Gasan.
+email-address: oklcs@naver.com
+![Alt text](./images.jpg "Made by React-native")
